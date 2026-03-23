3b3694173c3f6623295f1c0d9e70d220e8b26329
Murilo <murilo.breda@aluno.senai.br>
2026-03-23 08:02:55 -0300

    primeiro commit

---
+++ mvcAluno/resources/views/listar.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="pt-BR">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Relatório de Alunos</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>EMAIL</th>
+                <th>Atualizar</th>
+                <th>Deletar</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($alunos as $aluno);
+                <tr>
+                    <td>{{ $aluno->id }}</td>
+                    <td>{{ $aluno->nome }}</td>
+                    <td>{{ $aluno->email }}</td>
+                    <td>Faremos na proxima aula</td>
+                    <td>Faremos na proxima aula</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="3">Nenhum ALUNO encontrado</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+    
+</body>
+</html>
---
+++ mvcAluno/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/aluno/listar',[AlunoController::class, 'listar'])->name('aluno.listar');
+
+Route::get('/aluno/cadastrar', function(){
+    return view('cadastro');
+
+})->name('aluno.cadastro');
+
+//POST - enviar os dados para cadastrar usuarios
+Route::post('/aluno/salvar',[AlunoController::class, 'add'])->name('aluno.salvar');
