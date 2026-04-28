b3f4ff33485cc54889ab6f7ebd7db027afe0f7f2
Murilo <murilo.breda@aluno.senai.br>
2026-04-28 12:00:01 -0300

    28/04/26

---
+++ mvcEmpresa/app/Models/Departamento.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+Class Departamento extends Model{
+
+    protected $fillable = [
+        'setor',
+        'orcamento',
+        'dataCriacao'
+    ];
+
+    public function funcionario(){
+        return $this->hasMany(Funcionario::class);
+    }
+}
+
+?>
---
+++ mvcEmpresa/app/Models/dadosPessoais.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+Class dadosPessoais extends Model{
+
+    protected $fillable = [
+        'CPF',
+        'RG',
+        'dataNascimento',
+        'CEP',
+        'funcionario_id'
+    ];
+
+    public function funcionario(){
+        return $this->belongsTo(Funcionario::class);
+    }
+}
+
+?>
